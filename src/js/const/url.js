80f9cbc96c4b9fd97e28fa8eb1c401154ae60a3f
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-29 00:45:58 -0200

    url relativa

--- src/js/const/url.js
+++ src/js/const/url.js
@@ -6,10 +6,10 @@ module.exports = {
 	eventos: "eventos.html",
 
 	//data
-	ESPACO: "https://dl.dropboxusercontent.com/u/786375/anchieta/site/data/espaco.json",
-	FILMES: "https://dl.dropboxusercontent.com/u/786375/anchieta/site/data/filmes.json",
-	CARDAPIO: "https://dl.dropboxusercontent.com/u/786375/anchieta/site/data/cardapio.json",
+	ESPACO: "data/espaco.json",
+	FILMES: "data/filmes.json",
+	CARDAPIO: "data/cardapio.json",
 
 	//files
-	header: "https://dl.dropboxusercontent.com/u/786375/anchieta/site/imgs/header.jpg"
+	header: "imgs/header.jpg"
 };
